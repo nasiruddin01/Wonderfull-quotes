a6362878bbe4e8ff499084d3c445d315e4bcd9ea
nasiruddin01 <nasiruddin.dev@gmail.com>
2020-09-27 04:05:45 +0600

    master

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,48 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @quoteAdded="NewQuote"></app-new-quote>
+    <app-quote-grid
+      :quotes="quotes"
+      @quoteDeleted="deleteQuote"
+    ></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a Quote to Delete it!</div>
+      </div>
     </div>
-    <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
+import Header from "./components/Header.vue";
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  data: function() {
+    return {
+      quotes: ["Just a Quote to see some thing"],
+      maxQuotes: 10
+    };
+  },
+  methods: {
+    NewQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert("Please delete Quotes first!");
+      }
+      this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
     }
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header
   }
-}
-</style>
+};
+</script>
+
+<style lang="scss"></style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row">
+    <addQuote
+      v-for="(quote, index) in quotes"
+      :key="index"
+      @click.native="deleteQuote(index)"
+      >{{ quote }}</addQuote
+    >
+  </div>
+</template>
+
+<script>
+import Quote from "./Quote.vue";
+export default {
+  props: ["quotes"],
+  components: {
+    addQuote: Quote
+  },
+  methods: {
+    deleteQuote(index) {
+      this.$emit("quoteDeleted", index);
+    }
+  }
+};
+</script>
+
+<style lang="scss"></style>
